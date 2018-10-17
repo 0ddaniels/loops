667209c023bc130f643a1ea831e4402693444e56
0ddaniels <42749711+0ddaniels@users.noreply.github.com>
2018-10-17 14:04:16 -0500

    Add files via upload

--- loops.js
+++ loops.js
@@ -1,21 +1,21 @@
 // For-loop functions
 
-function positiveIntSum(n){
+function positiveIntSum(limit){
     let sum = 0;
-    let n = 100;
-    for(let i = 1; i <= n; i++){
+    for(let i = 1; i <= limit; i++){
          sum = sum + i;
     }
-
+return sum;
 
 }
 
 
 function additionPatternSum(start, diff limit){
+    let sum = start;
     for(let i = start; i <= limit; i + diff){
         sum = sum + i;
     }
-
+return sum;
 
 }
 
@@ -24,8 +24,16 @@ function additionPatternSum(start, diff limit){
 
 // While-loop function
 
-function multiplicationPatternSum(/* Give it three inputs */){
+function multiplicationPatternSum(start, factor, max){
+    let sum = start;
+    let max = 100;
+    let factor = 2;
+    while(sum <= max){
+        start = start + factor;
+        factor = 2 * factor;
+    }
 
+return sum;
 
 }
 
@@ -34,3 +42,30 @@ function multiplicationPatternSum(/* Give it three inputs */){
 
 
 //Later copy paste the checking code below here.
+//Declarion of checking code
+function check(){
+    let smallLimit = 208;
+    let mediumLimit = 1024;
+    let largeLimit = 600000;//six hundred thousand
+
+    let test1 = 21736 === positiveIntSum(smallLimit);
+    let test2 = 180000300000 === positiveIntSum(mediumLimit);
+    let test3 = 1770 === additionPatternSum(60,3,20);
+    let test4 = 8375 === additionPatternSum(-200, 15, 50);
+    let test5 = 12345678 === multiplicationPatternSum(1,3,largeLimit);
+    let test6 = 12345678 === multiplicationPatternSum(5,2,largeLimit);
+    let test7 = 12345678 === multiplicationPatternSum(2,-7,largeLimit);
+
+    console.log(`The function positiveIntSum works properly: ${test1 && test2}`);
+    console.log(`The function additionPatternSum works properly: ${test3 && test4}`);
+    console.log(`The function positiveIntSum works properly: ${test5 && test6 && test7}`);
+    console.log(`-------------------------------------------------------------`);
+    console.log(`
+    Common mistakes to check
+    1. Did you spell everything correctly?
+    2. Does it actually return an answer?
+    3. Were your original notes correct?
+    `);
+}
+//Actually running the checking code.
+check();
