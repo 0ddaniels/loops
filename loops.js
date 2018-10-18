588099eb460b0b745d1b0e3fee71e5b87a99ea02
0ddaniels <42749711+0ddaniels@users.noreply.github.com>
2018-10-18 13:58:40 -0500

    Add files via upload

--- loops.js
+++ loops.js
@@ -10,10 +10,10 @@ return sum;
 }
 
 
-function additionPatternSum(start, diff limit){
+function additionPatternSum(start, diff, limit){
     let sum = start;
-    for(let i = start; i <= limit; i + diff){
-        sum = sum + i;
+    for(let k = 1; k <= limit; k++){
+        sum = sum + diff*k;
     }
 return sum;
 
@@ -26,11 +26,9 @@ return sum;
 
 function multiplicationPatternSum(start, factor, max){
     let sum = start;
-    let max = 100;
-    let factor = 2;
-    while(sum <= max){
-        start = start + factor;
-        factor = 2 * factor;
+    let k = start;
+    while(k <= max){
+        sum = sum + factor*k;
     }
 
 return sum;
@@ -43,29 +41,6 @@ return sum;
 
 //Later copy paste the checking code below here.
 //Declarion of checking code
-function check(){
-    let smallLimit = 208;
-    let mediumLimit = 1024;
-    let largeLimit = 600000;//six hundred thousand
-
-    let test1 = 21736 === positiveIntSum(smallLimit);
-    let test2 = 180000300000 === positiveIntSum(mediumLimit);
-    let test3 = 1770 === additionPatternSum(60,3,20);
-    let test4 = 8375 === additionPatternSum(-200, 15, 50);
-    let test5 = 12345678 === multiplicationPatternSum(1,3,largeLimit);
-    let test6 = 12345678 === multiplicationPatternSum(5,2,largeLimit);
-    let test7 = 12345678 === multiplicationPatternSum(2,-7,largeLimit);
-
-    console.log(`The function positiveIntSum works properly: ${test1 && test2}`);
-    console.log(`The function additionPatternSum works properly: ${test3 && test4}`);
-    console.log(`The function positiveIntSum works properly: ${test5 && test6 && test7}`);
-    console.log(`-------------------------------------------------------------`);
-    console.log(`
-    Common mistakes to check
-    1. Did you spell everything correctly?
-    2. Does it actually return an answer?
-    3. Were your original notes correct?
-    `);
-}
-//Actually running the checking code.
-check();
+console.log(positiveIntSum(2000000))
+console.log(additionPatternSum(-80, 60, 50))
+console.log(multiplicationPatternSum(4, 3, 50000))
